0b4fea343511b3b4abc47aa8c92cb6ef3803789a
VasilevSaryal <vasilyev.s@indriver.com>
2020-06-19 16:40:17 +0900

    Most Bug Fix

--- jXnet/KanaBranch/ExercisesViewController.swift
+++ jXnet/KanaBranch/ExercisesViewController.swift
@@ -43,8 +43,6 @@ class ExercisesViewController: UIViewController, UITableViewDelegate, UITableVie
     var showAnswer12: UIButton?
     //Экстренный костыль
     var becomeLevel: UILabel!
-    //Начать заново
-    private var repeatButton: UIButton!
     //Перекрывания экрана используются для неправильного нажатия
     private let fullScreenView : UIView = {
         let view = UIView()
@@ -56,7 +54,6 @@ class ExercisesViewController: UIViewController, UITableViewDelegate, UITableVie
     private var showKana: ShowKana!
     private var comparsionTask: ComparsionTask!
     private var handwritingView: HandwritingView!
-    private var resultView: ResultView!
     
     //Таблица результатов
     private let resultTable: UITableView = {
@@ -125,9 +122,11 @@ class ExercisesViewController: UIViewController, UITableViewDelegate, UITableVie
     }
     
     func initialParameters() -> Void {
-        self.view.layer.sublayers?.forEach {$0.removeAllAnimations()}
-        self.view.layer.sublayers?.forEach {$0.removeFromSuperlayer()}
+
         self.view.subviews.forEach { $0.removeFromSuperview() }//Удаление всех элементов
+        self.view.layer.sublayers?.forEach {$0.removeFromSuperlayer()}
+        self.view.layer.sublayers?.forEach {$0.removeAllAnimations()}
+        
         self.view.addSubview(timerProgress)
         pairInt.removeAll()
         if (self.lessonNumber == 4 || self.lessonNumber == 5) {
@@ -141,7 +140,6 @@ class ExercisesViewController: UIViewController, UITableViewDelegate, UITableVie
         ask.removeAll()
         checkedAnswers.removeAll()
         
-        resultView = ResultView.init(self)
         if courseNumber == 1 {
             //Инициализация после прогрузки данного ViewController
             showKana = ShowKana.init(self)
@@ -701,9 +699,36 @@ class ExercisesViewController: UIViewController, UITableViewDelegate, UITableVie
         for answer in incorrectAnswers {
             if answer == false {correctSum += 1}
         }
-        self.view = resultView.drawResult(correctAnswer: correctSum, countQuestion: countQuestion, totalScore: 7500)
-        //self.resultTableDraw()
-
+        drawSuperResult(correctAnswer: correctSum, countQuestion: countQuestion, totalScore: 7500)
+        self.resultTableDraw()
+        self.resultTable.isHidden = true
+        //Repeat button
+        let repeatButtonq = UIButton(frame: CGRect(x: 0, y: UIScreen.main.bounds.height - 54, width: UIScreen.main.bounds.width, height: 54))
+        repeatButtonq.setTitle("Попробовать еще раз", for: .normal)
+        repeatButtonq.setTitleColor(.white, for: .normal)
+        repeatButtonq.backgroundColor = UIColor.init(hexFromString: "#FF3300")
+        repeatButtonq.addTarget(nil, action: #selector(repeatAction), for: .touchUpInside)
+        self.view.addSubview(repeatButtonq)
+        //switch result
+        let items = ["Результат", "Статистика"]
+        let switcher = UISegmentedControl(items: items)
+        switcher.frame = CGRect(x: (UIScreen.main.bounds.width - 160) / 2, y: repeatButtonq.frame.minY - 40, width: 160, height: 30)
+        switcher.selectedSegmentIndex = 0
+        // Style the Segmented Control
+        switcher.layer.cornerRadius = 5.0  // Don't let background bleed
+        switcher.tintColor = UIColor(hexFromString: "#FF3300")
+        
+        switcher.addTarget(self, action: #selector(changeResult), for: .valueChanged)
+        self.view.addSubview(switcher)
+    }
+    
+    @objc func changeResult(sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            self.resultTable.isHidden = true
+        default:
+            self.resultTable.isHidden = false
+        }
     }
     
     private func resultTableDraw() {
@@ -715,7 +740,7 @@ class ExercisesViewController: UIViewController, UITableViewDelegate, UITableVie
         resultTable.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             resultTable.topAnchor.constraint(equalTo: (navigationController?.navigationBar.topAnchor)!, constant: 0),
-            resultTable.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: -54),
+            resultTable.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: -115),
             resultTable.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
             resultTable.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0)
         ])
@@ -867,6 +892,222 @@ class ExercisesViewController: UIViewController, UITableViewDelegate, UITableVie
             }
         }
         return uniqueNumbers.shuffled()
+    }
+    
+    // MARK: - ResultView
+    
+    let scoreLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 30)
+        label.textAlignment = .center
+        return label
+    }()
+    let scoreLabelCondition = UILabel()
+    let trackLayer = CAShapeLayer()
+    
+    let percentLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 30)
+        label.textAlignment = .center
+        return label
+    }()
+    let percentLabelCondition = UILabel()
+    let shapeLayer2 = CAShapeLayer()
+    
+    let levelProgressView = UIProgressView(progressViewStyle: .default)
+    var newWidth: CGFloat!
+    
+    func drawSuperResult(correctAnswer: Int, countQuestion: Int, totalScore: Int) {
+        self.title = "Результат"
+        //TotalResutl
+        let totalResult = UILabel(frame: CGRect(x: 20, y: self.topbarHeight + 1, width: UIScreen.main.bounds.width - 40, height: 40))
+        totalResult.text = "Результат: \(correctAnswer)/\(countQuestion)"
+        totalResult.textColor = .black
+        totalResult.font = UIFont.systemFont(ofSize: 27)
+        totalResult.adjustsFontSizeToFitWidth = true
+        totalResult.textAlignment = .center
+        totalResult.minimumScaleFactor = 0.4
+        self.view.addSubview(totalResult)
+        
+        //ResultComment
+        let resultComment = UILabel(frame: CGRect(x: 20, y: totalResult.frame.maxY + 1, width: UIScreen.main.bounds.width - 40, height: 60))
+        resultComment.textColor = .black
+        resultComment.font = UIFont.systemFont(ofSize: 21)
+        resultComment.adjustsFontSizeToFitWidth = true
+        resultComment.textAlignment = .center
+        resultComment.minimumScaleFactor = 0.4
+        resultComment.numberOfLines = 2
+        resultComment.lineBreakMode = .byWordWrapping
+        resultComment.text = "У вас определенный талант к этому!"
+        self.view.addSubview(resultComment)
+        
+        //Score Track
+        // create my track layer
+        let circularPath = UIBezierPath(arcCenter: .init(x: (UIScreen.main.bounds.width - 60) * 3 / 4 + 40, y: resultComment.frame.maxY + 40 + (UIScreen.main.bounds.width - 60) / 4), radius: (UIScreen.main.bounds.width - 60) / 4, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
+        trackLayer.path = circularPath.cgPath
+        trackLayer.strokeColor = UIColor.lightGray.cgColor
+        trackLayer.lineWidth = 5
+        trackLayer.lineCap = .round
+        trackLayer.fillColor = UIColor.clear.cgColor
+        self.view.layer.addSublayer(trackLayer)
+        
+        scoreLabel.frame = CGRect(x: 0, y: 0, width: (UIScreen.main.bounds.width - 60) / 2, height: 30)
+        scoreLabel.center = CGPoint(x: (UIScreen.main.bounds.width - 60) * 3 / 4 + 40, y: resultComment.frame.maxY + 35 + (UIScreen.main.bounds.width - 60) / 4)
+        scoreLabel.textColor = UIColor(hexFromString: "#33CC66")
+        self.view.addSubview(scoreLabel)
+        
+        scoreLabelCondition.frame = CGRect(x: scoreLabel.frame.minX, y: scoreLabel.frame.maxY + 5, width: (UIScreen.main.bounds.width - 60) / 2, height: 13)
+        scoreLabelCondition.textAlignment = .center
+        scoreLabelCondition.textColor = .darkGray
+        scoreLabelCondition.font = .systemFont(ofSize: 10)
+        self.view.addSubview(scoreLabelCondition)
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = circularPath.cgPath
+        shapeLayer.strokeColor = UIColor(hexFromString: "#33CC66").cgColor
+        shapeLayer.lineWidth = 5
+        shapeLayer.strokeEnd = 0
+        shapeLayer.lineCap = .round
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        self.view.layer.addSublayer(shapeLayer)
+        
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        basicAnimation.toValue = 1
+        basicAnimation.duration = 2
+        basicAnimation.fillMode = .forwards
+        basicAnimation.isRemovedOnCompletion = false
+        shapeLayer.add(basicAnimation, forKey: "urSoBasic")
+        
+        //Percent Track
+        // create my track layer
+        let trackLayer2 = CAShapeLayer()
+        let circularPath2 = UIBezierPath(arcCenter: .init(x: (UIScreen.main.bounds.width - 60) / 4 + 20, y: resultComment.frame.maxY + 40 + (UIScreen.main.bounds.width - 60) / 4), radius: (UIScreen.main.bounds.width - 60) / 4, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
+        trackLayer2.path = circularPath2.cgPath
+        trackLayer2.strokeColor = UIColor.lightGray.cgColor
+        trackLayer2.lineWidth = 5
+        trackLayer2.lineCap = .round
+        trackLayer2.fillColor = UIColor.clear.cgColor
+        self.view.layer.addSublayer(trackLayer2)
+        
+        percentLabel.frame = CGRect(x: 0, y: 0, width: (UIScreen.main.bounds.width - 60) / 2, height: 30)
+        percentLabel.center = CGPoint(x: (UIScreen.main.bounds.width - 60) / 4 + 20, y: resultComment.frame.maxY + 35 + (UIScreen.main.bounds.width - 60) / 4)
+        percentLabel.textColor = .blue
+        self.view.addSubview(percentLabel)
+        
+        percentLabelCondition.frame = CGRect(x: percentLabel.frame.minX, y: percentLabel.frame.maxY + 5, width: (UIScreen.main.bounds.width - 60) / 2, height: 13)
+        percentLabelCondition.textAlignment = .center
+        percentLabelCondition.textColor = .darkGray
+        percentLabelCondition.font = .systemFont(ofSize: 10)
+        self.view.addSubview(percentLabelCondition)
+        
+        
+        shapeLayer2.path = circularPath2.cgPath
+        shapeLayer2.strokeColor = UIColor.blue.cgColor
+        shapeLayer2.lineWidth = 5
+        shapeLayer2.strokeEnd = 0
+        shapeLayer2.lineCap = .round
+        shapeLayer2.fillColor = UIColor.clear.cgColor
+        self.view.layer.addSublayer(shapeLayer2)
+        
+        let basicAnimation2 = CABasicAnimation(keyPath: "strokeEnd")
+        basicAnimation2.toValue = 1
+        basicAnimation2.duration = 2
+        basicAnimation2.fillMode = .forwards
+        basicAnimation2.isRemovedOnCompletion = false
+        shapeLayer2.add(basicAnimation2, forKey: "urSoBasic")
+        
+        //Level label
+        let levelLabel = UILabel(frame: CGRect(x: 20, y: resultComment.frame.maxY + 60 + (UIScreen.main.bounds.width - 60) / 2, width: UIScreen.main.bounds.width - 40, height: 40))
+        levelLabel.textColor = .black
+        levelLabel.font = UIFont.systemFont(ofSize: 21)
+        levelLabel.adjustsFontSizeToFitWidth = true
+        levelLabel.textAlignment = .left
+        levelLabel.minimumScaleFactor = 0.4
+        self.view.addSubview(levelLabel)
+        
+        //Current level Progress View
+        let growLevel = UILabel(frame: CGRect(x: 20, y: levelLabel.frame.maxY + 3, width: 1, height: 20))
+        growLevel.backgroundColor = UIColor.init(hexFromString: "#33CC66")
+        self.view.addSubview(growLevel)
+        //growLevel.removeFromSuperview()
+        self.becomeLevel = UILabel()
+        self.becomeLevel.frame = CGRect(x: 20, y: levelLabel.frame.maxY + 3, width: 1, height: 20)
+        self.becomeLevel.backgroundColor = UIColor(hexFromString: "#DE6FA1")
+        self.becomeLevel.isHidden = true
+        self.view.addSubview(self.becomeLevel)
+        
+        var userLevel = UserDefaults.standard.integer(forKey: "userLevel")
+        levelLabel.text = "Уровень: \(userLevel)"
+        var experienceCount = UserDefaults.standard.integer(forKey: "userExperience")
+        var nextLevel = UserDefaults.standard.integer(forKey: "nextLevel")
+        let stepLevel = UserDefaults.standard.integer(forKey: "stepLevel")
+        
+        //Experience label
+        let experienceLabel = UILabel(frame: CGRect(x: UIScreen.main.bounds.width - 120, y: resultComment.frame.maxY + 60 + (UIScreen.main.bounds.width - 60) / 2, width: 80, height: 40))
+        experienceLabel.textColor = .black
+        experienceLabel.adjustsFontSizeToFitWidth = true
+        experienceLabel.textAlignment = .right
+        experienceLabel.font = .systemFont(ofSize: 16)
+        experienceLabel.text = "+\(totalScore)"
+        experienceLabel.textColor = UIColor.init(hexFromString: "#33CC66")
+        experienceLabel.minimumScaleFactor = 0.4
+        self.view.addSubview(experienceLabel)
+        
+        growLevel.frame.size.width = (UIScreen.main.bounds.width - 40) * (CGFloat(experienceCount) / CGFloat(nextLevel))
+        
+        if (experienceCount + totalScore) >= nextLevel {
+            userLevel += 1
+            experienceCount = (experienceCount + totalScore) - nextLevel
+            nextLevel += stepLevel
+            newWidth = (UIScreen.main.bounds.width - 40) * (CGFloat(experienceCount) / CGFloat(nextLevel))
+            UIView.animate(withDuration: 1, animations: {
+                if (growLevel.superview != nil) {
+                    growLevel.frame.size.width = (UIScreen.main.bounds.width - 40)
+                }
+            }, completion: {
+                (value: Bool) in
+                if (growLevel.superview != nil) {
+                    growLevel.frame.size.width = 1
+                    growLevel.frame.size.height = 20
+                    levelLabel.text = "Уровень: 2"
+                }
+            })
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0, execute: {
+                UIView.animate(withDuration: 1, animations: {
+                    if (growLevel.superview != nil) {
+                        growLevel.frame.size.width = self.newWidth
+                    }
+                }, completion: {
+                    (value: Bool) in
+                    if (growLevel.superview != nil) {
+                        experienceLabel.isHidden = true
+                        growLevel.removeFromSuperview()
+                    }
+                })
+            })
+        } else {
+            experienceCount += totalScore
+            newWidth = (UIScreen.main.bounds.width - 40) * (CGFloat(experienceCount) / CGFloat(nextLevel))
+            UIView.animate(withDuration: 2, animations: {
+                growLevel.frame.size.width = self.newWidth
+            }, completion: {
+                (value: Bool) in
+                if (growLevel.superview != nil) {
+                    growLevel.removeFromSuperview()
+                }
+            })
+        }
+            
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.8, execute: {
+            if (self.scoreLabel.superview != nil) {
+                self.scoreLabel.text = "350"
+                self.scoreLabelCondition.text = "общий счёт"
+                self.percentLabel.text = "100%"
+                self.percentLabelCondition.text = "точность выполнения"
+                self.becomeLevel.frame.size.width = self.newWidth
+                self.becomeLevel.isHidden = false
+            }
+        })
     }
 }
 
